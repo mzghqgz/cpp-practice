1d6841a7bc3d28927283b77bd7f81660215c5471
mzghqgz <l13177037245@Outlook.com>
2026-03-15 16:03:33 +0800

    49.最大子数组的和

--- C++/刷题.cpp
+++ C++/刷题.cpp
@@ -2184,33 +2184,60 @@ struct TreeNode {
 
 
 
-//48.不相交的线
+////48.不相交的线
+//class Solution {
+//public:
+//    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+//        vector<vector<int>> dp(nums1.size() + 1, vector<int>(nums2.size() + 1, 0));
+//        for (int i = 1; i <= nums1.size(); i++)
+//        {
+//            for (int j = 1; j <= nums2.size(); j++)
+//            {
+//                if (nums1[i - 1] == nums2[j - 1])
+//                {
+//                    dp[i][j] = dp[i - 1][j - 1] + 1;
+//                }
+//                else
+//                {
+//                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+//                }
+//            }
+//        }
+//        return dp[nums1.size()][nums2.size()];
+//    }
+//};
+//int main()
+//{
+//    Solution s;
+//    vector<int> nums1 = { 2,5,1,2,5 };
+//    vector<int> nums2 = { 10,5,2,1,5,2 };
+//    s.maxUncrossedLines(nums1, nums2);
+//    return 0;
+//}
+
+
+
+
+
+//49.最大子数组的和
 class Solution {
 public:
-    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        vector<vector<int>> dp(nums1.size() + 1, vector<int>(nums2.size() + 1, 0));
-        for (int i = 1; i <= nums1.size(); i++)
+    int maxSubArray(vector<int>& nums) {
+        vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+        int result = dp[0];
+        for (int i = 1; i < nums.size(); i++)
         {
-            for (int j = 1; j <= nums2.size(); j++)
-            {
-                if (nums1[i - 1] == nums2[j - 1])
-                {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                }
-                else
-                {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
+            dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+            result = max(result, dp[i]);
         }
-        return dp[nums1.size()][nums2.size()];
+        return result;
     }
 };
 int main()
 {
     Solution s;
-    vector<int> nums1 = { 2,5,1,2,5 };
-    vector<int> nums2 = { 10,5,2,1,5,2 };
-    s.maxUncrossedLines(nums1, nums2);
+    vector<int> nums = { 5,4,-1,7,8 };
+    s.maxSubArray(nums);
     return 0;
 }
