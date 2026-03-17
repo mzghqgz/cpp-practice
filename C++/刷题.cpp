19a44c9aa687ca7059e0faf8ee0a17023fd5c19c
mzghqgz <l13177037245@Outlook.com>
2026-03-17 14:17:28 +0800

    49.最大子数组的和

--- C++/刷题.cpp
+++ C++/刷题.cpp
@@ -2219,25 +2219,103 @@ struct TreeNode {
 
 
 
-//49.最大子数组的和
+////49.最大子数组的和
+//class Solution {
+//public:
+//    int maxSubArray(vector<int>& nums) {
+//        vector<int> dp(nums.size(), 0);
+//        dp[0] = nums[0];
+//        int result = dp[0];
+//        for (int i = 1; i < nums.size(); i++)
+//        {
+//            dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+//            result = max(result, dp[i]);
+//        }
+//        return result;
+//    }
+//};
+//int main()
+//{
+//    Solution s;
+//    vector<int> nums = { 5,4,-1,7,8 };
+//    s.maxSubArray(nums);
+//    return 0;
+//}
+
+
+
+
+
+////50.判断子序列
+//class Solution {
+//public:
+//    bool isSubsequence(string s, string t) {
+//        vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0));
+//        if (s.size() > t.size())
+//        {
+//            return false;
+//        }
+//        for (int i = 1; i <= s.size(); i++)
+//        {
+//            for (int j = 1; j <= t.size(); j++)
+//            {
+//                if (s[i - 1] == t[j - 1])
+//                {
+//                    dp[i][j] = dp[i - 1][j - 1] + 1;
+//                }
+//                else
+//                {
+//                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+//                }
+//            }
+//        }
+//        return dp[s.size()][t.size()] == s.size();
+//    }
+//};
+//int main()
+//{
+//    Solution s;
+//    s.isSubsequence("abc", "ahbgdc");
+//    return 0;
+//}
+
+
+
+
+
+//51.不同的子序列
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        vector<int> dp(nums.size(), 0);
-        dp[0] = nums[0];
-        int result = dp[0];
-        for (int i = 1; i < nums.size(); i++)
+    int numDistinct(string s, string t) {
+        vector<vector<uint64_t>> dp(s.size() + 1, vector<uint64_t>(t.size() + 1));
+        for (int i = 0; i < s.size(); i++)
         {
-            dp[i] = max(dp[i - 1] + nums[i], nums[i]);
-            result = max(result, dp[i]);
+            dp[i][0] = 1;
         }
-        return result;
+        for (int j = 1; j < t.size(); j++)
+        {
+            dp[0][j] = 0;
+        }
+        for (int i = 1; i <= s.size(); i++)
+        {
+            for (int j = 1; j <= t.size(); j++)
+            {
+                if (s[i - 1] == t[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                }
+                else
+                {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[s.size()][t.size()];
     }
 };
 int main()
 {
     Solution s;
-    vector<int> nums = { 5,4,-1,7,8 };
-    s.maxSubArray(nums);
+    s.numDistinct("rabbbit", "rabbit");
     return 0;
 }
